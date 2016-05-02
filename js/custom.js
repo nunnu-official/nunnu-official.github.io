4c00154faf5aa7494c03115c3dd526816d14b8da
nunnu-official <nunnuofficial@gmail.com>
2016-05-02 22:13:13 +0900

    mod tooptip & clipboard

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 $(document).ready( function() {
 
-  $('.mail').darkTooltip({
-    trigger:'click',
+  $('.btn').darkTooltip({
+    trigger:'hover',
     gravity:'north'
+  });
+
+  $(function () {
+    var clipboard = new Clipboard('.btn');
+    clipboard.on('success', function(e) {
+      console.info('Action:', e.action);
+      console.info('Text:', e.text);
+      console.info('Trigger:', e.trigger);
+      e.clearSelection();
+    });
   });
 
 });
